0acb6b634fb77415349825d20a450083ca164a7c
이승열 <marudev4905@gmail.com>
2021-06-09 18:06:23 +0900

    main publishing ing

---
+++ js/slide.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".intro-slide", {
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    loop:true,
+    allowTouchMove: false,
+    speed:1000
+});
