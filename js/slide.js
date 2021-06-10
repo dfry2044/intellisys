0b6a7bf8d862533dac93fced3baad66a092952f1
이승열 <marudev4905@gmail.com>
2021-06-10 15:40:44 +0900

    section1, section2 퍼퍼블리싱 완료

--- js/slide.js
+++ js/slide.js
@@ -1,10 +1,33 @@
-var swiper = new Swiper(".intro-slide", {
-    centeredSlides: true,
+var interleaveOffset = 0.5;
+
+var swiperOptions = {
     autoplay: {
-        delay: 3500,
+        delay: 3000,
         disableOnInteraction: false,
     },
     loop:true,
     allowTouchMove: false,
-    speed:1000
-});
+    speed: 1000,
+    on: {
+        progress: function() {
+            var swiper = this;
+            for (var i = 0; i < swiper.slides.length; i++) {
+                var slideProgress = swiper.slides[i].progress;
+                var innerOffset = swiper.width * interleaveOffset;
+                var innerTranslate = slideProgress * innerOffset;
+                swiper.slides[i].querySelector(".slide-inner").style.transform =
+                "translate3d(" + innerTranslate + "px, 0, 0)";
+            }      
+        },
+        setTransition: function(speed) {
+            var swiper = this;
+            for (var i = 0; i < swiper.slides.length; i++) {
+                swiper.slides[i].style.transition = speed + "ms";
+                swiper.slides[i].querySelector(".slide-inner").style.transition =
+                speed + "ms";
+            }
+        }
+    }
+};
+
+var swiper = new Swiper(".swiper-container", swiperOptions);
