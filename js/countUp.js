bf245b302abfb193e7b91f1e5dc278412d0262a3
천지연 <cheonz06@naver.com>
2021-06-14 15:05:00 +0900

    count up 수정 중

--- js/countUp.js
+++ js/countUp.js
@@ -1,32 +1,15 @@
-function counterUp(element, startAtPercentage) {
-    if (startAtPercentage == null) {
-        startAtPercentage = 0;
+$(function(){
+    var cnt0 = 0;
+    counterFn();
+    function counterFn() {
+        id0 = setInterval(counter0Fn, 50);
+        function counter0Fn()  {
+            cnt0++;
+            if(cnt0 > 300) {
+                clearInterval(id0);
+            }else{
+                $(".counter1").text(cnt0);
+            }
+        }
     }
-
-    var numericValue = getNumericValue(element);
-
-    jQuery(element).counterUp({
-        beginAt: (numericValue > 0)
-            ? (numericValue * startAtPercentage / 100)
-            : undefined,
-    });
-}
-
-function getNumericValue(element) {
-    try {
-        return Number(element.innerText
-            // Remove decimal points
-            .replace(/.\D+/g, '')
-            // Remove thousands separator and other symbols
-            .replace(/\D/g, ''));
-    } catch (error) {
-        return 0;
-    }
-}
-
-// Apply it to all elements with vanilla JavaScript (ES5)
-Array.prototype.forEach.call(
-    document.querySelectorAll('.countAnimation'),
-    function (element) {
-        counterUp(element, 0);
-    });
+});
