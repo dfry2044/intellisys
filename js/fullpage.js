33f2cb7e8d25942bd065ce6cdf2aeee910c84a4e
천지연 <cheonz06@naver.com>
2021-06-15 09:59:21 +0900

    카운트업 수정중

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,8 +1,52 @@
 $(document).ready(function(){
+    
+    var cnt = 0;
+    var counterNum = 1;
+    var counterLen = $(".counter").length;
+    function countUp(number){
+        counterFn(number);
+        if(counterNum != counterLen){
+            counterNum++;
+        }else{
+            counterNum = 0;
+        }
+    };
+    function counterFn(number) {
+        var id = setInterval(function(){
+            if(cnt > number) {
+                clearInterval(id);
+            }else{
+                console.log("================")
+                console.log(counterNum);
+                console.log(number);
+                console.log(cnt);
+                console.log("================")
+                $(".counter").eq(counterNum).text(cnt);
+            }
+            cnt++;
+        }, 20);
+    }
+
     $("#fullpage").fullpage({
         navigation: true,
         navigationPosition: 'right', 
         scrollingSpeed: 1000,
         scrollOverflow: true,
+        onLeave:function(index){
+            if(index == 4){
+                setTimeout(function(){
+                    countUp(300);
+                },0)
+                setTimeout(function(){
+                    countUp(18);
+                },1500)
+                // setTimeout(function(){
+                //     countUp(12);
+                // },25000000000)
+                // setTimeout(function(){
+                //     countUp(90);
+                // },25000000000)
+            }
+        }
     })
 })
