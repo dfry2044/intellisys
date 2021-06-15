b916435b80eb71259ecfc729007eb3370433c0f2
이승열 <marudev4905@gmail.com>
2021-06-15 18:06:24 +0900

    countUp.js 추가

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,52 +1,47 @@
 $(document).ready(function(){
-    
-    var cnt = 0;
-    var counterNum = 1;
-    var counterLen = $(".counter").length;
-    function countUp(number){
-        counterFn(number);
-        if(counterNum != counterLen){
-            counterNum++;
-        }else{
-            counterNum = 0;
+    var countArray = [300, 18, 12, 90];
+    var countObject = {
+        countUp : function(i,count0, count1, count2, count3){
+            eval(`count${i}.start();`)
         }
     };
-    function counterFn(number) {
-        var id = setInterval(function(){
-            if(cnt > number) {
-                clearInterval(id);
-            }else{
-                console.log("================")
-                console.log(counterNum);
-                console.log(number);
-                console.log(cnt);
-                console.log("================")
-                $(".counter").eq(counterNum).text(cnt);
-            }
-            cnt++;
-        }, 20);
+    for(var i = 0; i < countArray.length; i++){
+        eval(`var count${i} = new countUp.CountUp("count${i}",${countArray[i]},{startVal:0})`);
     }
-
     $("#fullpage").fullpage({
         navigation: true,
         navigationPosition: 'right', 
         scrollingSpeed: 1000,
         scrollOverflow: true,
         onLeave:function(index){
+            // if(index == 4){
+            //     for(var i = 0; i < countArray.length; i++){
+            //         eval(`count${i}.reset();`)
+            //     }
+            // }
             if(index == 4){
-                setTimeout(function(){
-                    countUp(300);
-                },0)
-                setTimeout(function(){
-                    countUp(18);
-                },1500)
-                // setTimeout(function(){
-                //     countUp(12);
-                // },25000000000)
-                // setTimeout(function(){
-                //     countUp(90);
-                // },25000000000)
+                var i = 0;
+                var countInterval = setInterval(function(){
+                    countUpFunc(i);
+                    i++;
+                    if(i >= countArray.length){
+                        clearInterval(countInterval);
+                    }
+                },500)
+            }
+            if(index == 5){
+                var i = 0;
+                var countInterval = setInterval(function(){
+                    countUpFunc(i);
+                    i++;
+                    if(i >= countArray.length){
+                        clearInterval(countInterval);
+                    }
+                },500)
             }
         }
     })
+    function countUpFunc(i){
+        countObject.countUp(i, count0, count1, count2, count3)
+    }
 })
