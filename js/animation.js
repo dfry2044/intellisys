41db48a6c121a03d21e9136ed8c389d8bba62fae
이승열 <marudev4905@gmail.com>
2021-07-19 17:13:01 +0900

    반응형 1920, 1500, 1280 코드 추가

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,7 @@
 var secTitle            = document.querySelectorAll(".sec-title");
 var diagramImg          = document.querySelectorAll(".sec-diagram");
+var sideNav             = document.querySelector(".page-side");
+var visualSec           = document.querySelector(".page-visual");
 var winY; 
 var titleTop;
 var imgTop;
@@ -9,15 +11,14 @@ $(window).scroll(function(){
 
     secTitle.forEach(function(el, index, arr2){
         titleTop = el.getBoundingClientRect().top;
-        console.log(winY / 1.5);
-        if(winY / 1.5 >= titleTop){
+        if(winY / 2 >= titleTop){
             el.classList.add("show");
         }
     })
     diagramImg.forEach(function(el, index, arr2){
         imgTop = el.getBoundingClientRect().top;
 
-        if(winY / 1.5 >= imgTop){
+        if(winY / 2 >= imgTop){
             el.classList.add("show");
         }
     })
